ac3a1e503803a86b617cbeb7e3638ec91769d6d4
Grant Slattery <grantslattery@gmail.com>
2020-09-18 14:55:25 -0400

    Updates

---
+++ precache-manifest.8afb1174724ccac914789973534aded8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3621c225b876b927691cfa5da75829c5",
+    "url": "/projectSiteA/index.html"
+  },
+  {
+    "revision": "e8a7ff930fa8e30080a3",
+    "url": "/projectSiteA/static/css/main.7c500103.chunk.css"
+  },
+  {
+    "revision": "a32f2883f23a9e9a43f7",
+    "url": "/projectSiteA/static/js/2.71144f88.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/projectSiteA/static/js/2.71144f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a7ff930fa8e30080a3",
+    "url": "/projectSiteA/static/js/main.5332d172.chunk.js"
+  },
+  {
+    "revision": "b3e57213019683a3468e",
+    "url": "/projectSiteA/static/js/runtime-main.52a219ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projectSiteA/static/media/logo.5d5d9eef.svg"
+  }
+]);
